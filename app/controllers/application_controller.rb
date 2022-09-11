d4b5dff32bfa6a9ca0e13362600264611b86cc46
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-11 23:58:07 +0530

    Add user authentication using devise token auth Set change header on each request to false in devise_token_auth file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+				def get_current_user
+					if user_signed_in?
+            return current_user
+          end
+				end
 end
