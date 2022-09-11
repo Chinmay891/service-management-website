54ae44213661beb15eeaf665fd9eaf323e34ec7c
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-12 00:24:25 +0530

    Add routes for actions in appointments controller Add routes for actions in services controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index'
+  get '/services', to: 'services#index'
+  post '/services', to: 'services#create'
+  get '/appointments', to: 'appointments#index'
+  post '/appointments/:servce_id', to: 'appointments#create'
 end
