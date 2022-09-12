29cc7e40f621849e9561685cdc00d1298b15e1a4
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-12 16:14:32 +0530

    Update routes Add route for action update in service_provider_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index'
+ 
   get '/services', to: 'services#index'
   post '/services', to: 'services#create'
   get '/appointments', to: 'appointments#index'
-  post '/appointments/:servce_id', to: 'appointments#create'
+  post '/appointments/:id', to: 'appointments#create'
   get '/service_providers', to: 'service_providers#index'
+  patch '/service_providers/:id', to: 'service_providers#update'
 end
