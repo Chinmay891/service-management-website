444bee444819756f04d8f23d5bb7b0bddf80f3de
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-12 11:28:56 +0530

    Update indentation from 4 spaces 2 spaces

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,30 +1,28 @@
 class AppointmentsController < ApplicationController
-    before_action :authenticate_user!
+	before_action :authenticate_user!
 
-		def index
-			user = get_current_user
-			appointments = Appointment.where(user_id: user.id)
-			render json:{appointments: appointments}
+	def index
+		user = get_current_user
+		appointments = Appointment.where(user_id: user.id)
+		render json:{appointments: appointments},status: 200
+	end
+
+	def create
+		user = get_current_user
+		appointment = Appointment.new(
+			{
+				service_id: params[:servce_id],
+				user_id: user.id,
+				date: appointment_params[:date]
+			}
+		)
+
+		if appointment.save
+			render json: appointment,status: 200
+		else
+			render json:{message: "Unable to create appointment"},status: 400
 		end
-
-    def create
-			user = get_current_user
-			appointment = Appointment.new(
-				{
-					service_id: params[:servce_id],
-					user_id: user.id,
-					date: appointment_params[:date]
-				}
-			)
-
-			if appointment.save
-				render json: appointment
-			else
-				render json:{message: "Unable to create appointment"}
-			end
-    end
-
-		
+  end
 
 		private
 		def appointment_params
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,33 +1,33 @@
 class ServicesController < ApplicationController
-    before_action :authenticate_user! 
+	before_action :authenticate_user! 
 
-    def index
-        @services = Service.all
-        render json: @services
-    end
-    
-    def create
-        user = get_current_user
-        service = Service.new(
-            {
-                user_id: user.id,
-                name: service_parameters[:name],
-                area: service_parameters[:area],
-                description: service_parameters[:description],
-                charge: service_parameters[:charge]
-            }
-        )
+	def index
+		@services = Service.all
+		render json: @services, status: 200
+	end
+	
+	def create
+		user = get_current_user
+		service = Service.new(
+			{
+				user_id: user.id,
+				name: service_parameters[:name],
+				area: service_parameters[:area],
+				description: service_parameters[:description],
+				charge: service_parameters[:charge]
+			}
+		)
 
-        if service.save
-            render json: service
-        else
-            render json:{message: "Unable to create service"}
-        end
-    end
+		if service.save
+			render json: service, status: 200
+		else
+			render json:{message: "Unable to create service"}, status: 400
+		end
+	end
 
-    private
-    def service_parameters
-        params.permit(:name,:area,:description,:charge)
-    end
+	private
+	def service_parameters
+		params.permit(:name,:area,:description,:charge)
+	end
 
 end
