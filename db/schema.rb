0b37a82e54fea26d5923ff3bf8f3a50a24c92742
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-15 14:59:17 +0530

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_12_072305) do
+ActiveRecord::Schema.define(version: 2022_09_15_065104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,10 +51,10 @@ ActiveRecord::Schema.define(version: 2022_09_12_072305) do
     t.string "unconfirmed_email"
     t.string "name"
     t.string "email"
-    t.integer "contact_number"
     t.json "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "contact_number"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
