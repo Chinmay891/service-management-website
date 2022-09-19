dfc3fb9d87803ddfe2fa18b2e46d4fe9a67cdcb7
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-19 15:29:22 +0530

    Add validations to date attribute

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,15 @@
 class Appointment < ApplicationRecord
-    belongs_to :user
-    belongs_to :service
-    validates :date, presence: true, null: false
+	belongs_to :user
+	belongs_to :service
+	validates :date, presence: true, null: false
+	validate :valid_date?
+
+	private
+	def valid_date?
+		if date != nil
+			if date < DateTime.now
+				self.errors.add :date, "Please select a valid time"
+			end
+		end
+	end
 end
