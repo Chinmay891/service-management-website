f6272c1d76b5786d8c5670ab79433426edd0bfcc
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-19 15:37:40 +0530

    Create test cases and api documentation for services controller

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:service) {create(:service,description: "Best service")}
+
+  describe 'validations' do
+    context 'when name area and charge contains data' do
+      before do
+        @userObj = Service.create!(:user,name:'Plumber',area:'Balewadi',charge:1000)
+      end
+      it 'should validate data with no errors' do
+        @userObj.save
+        expect(@userObj.errors.full_messages).to be_empty
+      end
+    end
+
+    context 'when name area and charge does not contains data' do
+      before do
+        @userObj = Service.create!(:user,name:'',area:'',charge:)
+      end
+      it 'should validate data with errors' do
+        @userObj.save
+        expect(@userObj.errors.full_messages).to eq(["Name can't be blank"])
+      end
+    end
+
+  end
 end
