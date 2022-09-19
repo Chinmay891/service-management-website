3a70df55afa3ba47ee304f2da40dab0d0fce41db
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-19 15:26:46 +0530

    Update: Pass service id through body instead of params

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,21 +4,29 @@ class AppointmentsController < ApplicationController
 	def index
 		user = get_current_user
 		appointments = Appointment.where(user_id: user.id)
-		render json:{appointments: appointments},status: 200
+		if appointments.empty?
+			render json: {message: "No appointments scheduled!"}
+		else
+			render json:{appointments: appointments},status: 200
+		end
 	end
 
 	def create
 		user = get_current_user
 		appointment = Appointment.new(
 			{
-				service_id: params[:id],
+				service_id: appointment_params[:service_id],
 				user_id: user.id,
 				date: appointment_params[:date]
 			}
 		)
 
 		if appointment.save
-			render json: appointment,status: 200
+			render json:{
+				service_id: appointment.service_id,
+				user_id: user.id,
+				date: appointment.date.strftime('%Y-%m-%d %H:%M:%S')
+			},status: 200
 		else
 			render json:{message: "Unable to create appointment"},status: 400
 		end
@@ -26,6 +34,6 @@ class AppointmentsController < ApplicationController
 
 	private
 	def appointment_params
-		params.permit(:date)
+		params.permit(:date,:service_id)
 	end
 end
