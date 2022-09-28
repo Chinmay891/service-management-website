7915983612c5fc017dd52473764b858b357acdd1
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-28 14:35:41 +0530

    Improvise context for test cases

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  let(:service) {create(:service,description: "Best service")}
+  let!(:user) {User.create!(name: 'c',email:'c@gmail.com',password:123456,password_confirmation: 123456,contact_number: 9876543210)}
 
   describe 'validations' do
-    context 'when name area and charge contains data' do
+    context 'for a valid service' do
       before do
-        @userObj = Service.create!(:user,name:'Plumber',area:'Balewadi',charge:1000)
+        @userObj = Service.create!(name:'Plumber',area:'Balewadi',charge:1000,user_id:user.id)
       end
-      it 'should validate data with no errors' do
+      it 'should validate data with no errors for creating service' do
         @userObj.save
         expect(@userObj.errors.full_messages).to be_empty
       end
     end
 
-    context 'when name area and charge does not contains data' do
+    context 'for an invalid service' do
       before do
-        @userObj = Service.create!(:user,name:'',area:'',charge:)
+        @userObj = Service.create(description:'Best service',user_id:user.id)
       end
-      it 'should validate data with errors' do
+      it 'should validate data with errors for presence of name' do
         @userObj.save
-        expect(@userObj.errors.full_messages).to eq(["Name can't be blank"])
+        expect(@userObj.errors.full_messages).to eq(["Name can't be blank","Area can't be blank", "Charge can't be blank"])
       end
     end
-
   end
 end
