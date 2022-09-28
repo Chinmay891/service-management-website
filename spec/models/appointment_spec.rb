8389268f1b2f15f5d1e9c4a6405745ed4e3a6dbf
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-28 15:35:38 +0530

    Add test cases for appointment model

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) {User.create(name: 'c',email:'c@gmail.com',password:123456,password_confirmation: 123456,contact_number: 9876543210)}
+  let!(:service){Service.create(name: "x",area: "y",charge: 123,user_id: user.id)}
+
+  describe 'validations' do
+    context 'for a valid appointment' do
+      before do
+        @userObj = Appointment.create(service_id: service.id,user_id: user.id,date: DateTime.now+100)
+      end
+      it 'should validate data with no errors for booking appointment' do
+        @userObj.save
+        expect(@userObj.errors.full_messages).to be_empty
+      end
+    end
+
+    context 'for an invalid service' do
+      before do
+        @userObj = Appointment.create(service_id: service.id,user_id: user.id,date: nil)
+      end
+      it 'should validate data with errors for presence of date' do
+        @userObj.save
+        expect(@userObj.errors.full_messages).to eq(["Date can't be blank"])
+      end
+    end
+  end
 end
